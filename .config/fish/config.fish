0558a928e82294c64d6db344b0dff9f42c286248
Alexei Doell <alexei.doell@calian.com>
2025-07-18 08:47:32 -0600

    fix fish browser

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    tv init fish | source
     set -gx fish_prompt_pwd_dir_length 0
     set -g fish_greeting
+    set -gx BROWSER google-chrome
     set -gx EDITOR nvim
+    tv init fish | source
 end
