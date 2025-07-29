99dc56ac52534f60dcc2867d9101874e5eaef628
Alexei Doell <alexei.doell@calian.com>
2025-07-29 13:22:05 -0600

    change autocomplete user binds

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,12 @@
 function fish_user_key_bindings
     bind -e \f
-    bind \f 'accept-autosuggestion'
+    bind \f 'forward-char'
+    bind -e \v
+    bind \v 'up-or-search'
+    bind -e \n
+    bind \n 'complete'
+    bind -e \b
+    bind \b 'backward-char'
     bind -e \cF
     bind \cF 'clear-screen'
 end
