c0cf10bcecf7ef2af0ac53adce90d38b010d6160
Alexei Doell <alexei.doell@calian.com>
2025-07-29 15:39:55 -0600

    bash style conditions...

--- .config/fish/functions/ctrl_c.fish
+++ .config/fish/functions/ctrl_c.fish
@@ -1,5 +1,5 @@
 function ctrl_c
-    if string length -- (commandline) > 0
+    if test (string length -- (commandline)) -gt 0
         commandline -f cancel-commandline
     else
         commandline -f clear-screen
